dbc5c381fab26041f09036693f0cdfcb20f3edf7
pmatar28code <78227811+pmatar28code@users.noreply.github.com>
2021-08-20 13:03:54 -0500

    working

--- app/src/main/java/com/example/qrcodecamerareader/BarcodeResultBottomSheet.kt
+++ app/src/main/java/com/example/qrcodecamerareader/BarcodeResultBottomSheet.kt
@@ -1,7 +1,5 @@
 package com.example.qrcodecamerareader
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -9,8 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import org.jsoup.Jsoup
 import java.util.concurrent.Executors
 
 class BarcodeResultBottomSheet: BottomSheetDialogFragment() {
@@ -21,16 +19,19 @@ class BarcodeResultBottomSheet: BottomSheetDialogFragment() {
     ): View? = inflater.inflate(R.layout.bottom_sheet_barcode_data, container, false)
 
     //We will call this function to update the URL displayed
-    fun updateURL(url: String) {
-        fetchUrlMetaData(url) { title ->
-            view?.apply {
-                findViewById<TextView>(R.id.text_view_title)?.text = title
-                //findViewById<TextView>(R.id.text_view_desc)?.text = desc
-                findViewById<TextView>(R.id.text_view_link)?.text = url
-                findViewById<TextView>(R.id.text_view_visit_link).setOnClickListener { _ ->
-                    Intent(Intent.ACTION_VIEW).also {
-                        it.data = Uri.parse(url)
-                        startActivity(it)
+    fun updateURL(url: String?) {
+        if (url != null) {
+            fetchUrlMetaData(url) { title ->
+                view?.apply {
+                    findViewById<TextView>(R.id.text_view_title)?.text = "This is The Address:"
+                    //findViewById<TextView>(R.id.text_view_desc)?.text = desc
+                    findViewById<TextView>(R.id.text_view_link)?.text = title
+                    findViewById<TextView>(R.id.text_view_visit_link).setOnClickListener { _ ->
+                    Toast.makeText(requireContext(),"THIS IS WHErE I SET COPY OR STORE VALUE",Toast.LENGTH_SHORT).show()
+                    //Intent(Intent.ACTION_VIEW).also {
+                           // it.data = Uri.parse(url)
+                           // startActivity(it)
+                       // }
                     }
                 }
             }
